1fbab3659785b9879896fc91ede00014cd312fad
Santhosh <163711416+Santhos-h@users.noreply.github.com>
2025-03-11 15:49:50 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById("menu");
+let bmenu = document.getElementById("bmenu");
+
+function display() {
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
